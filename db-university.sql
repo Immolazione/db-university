2d3e9ecc118f5f73c32ae0c1662fc7c61f3b87ae
Immolazione <federico.deered@gmail.com>
2022-03-09 16:55:49 +0100

    add the rest of second section

--- db-university.sql
+++ db-university.sql
@@ -30,7 +30,10 @@
     SELECT COUNT(id), YEAR(`enrolment_date`) FROM `students` GROUP BY YEAR(`enrolment_date`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+    SELECT COUNT(office_number), `office_address` FROM `teachers` GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
+    SELECT COUNT(exam_id), `exam_id`, ROUND(AVG(`vote`),2) FROM `exam_student` GROUP BY `exam_id`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+    SELECT COUNT(name), `department_id` FROM `degrees` GROUP BY `department_id`;
