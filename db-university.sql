1686987d0aaaee750541789e3b99dc89a2bb3c25
Immolazione <federico.deered@gmail.com>
2022-03-10 16:25:36 +0100

    add third assignment

--- db-university.sql
+++ db-university.sql
@@ -24,6 +24,8 @@
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
     SELECT COUNT(id) FROM `teachers` WHERE `phone` IS NULL;
 
+--------------------------------------------------------------------------------------------------
+
 -- GROUP BY
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
@@ -37,3 +39,26 @@
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
     SELECT COUNT(id), `department_id` FROM `degrees` GROUP BY `department_id`;
+
+--------------------------------------------------------------------------------------------------
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+
